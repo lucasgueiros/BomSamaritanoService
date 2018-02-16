f89b450701dcf1ffb543efe4581f6d44ed47bf97
= <=>
2018-02-16 16:14:05 -0300

    Fix copyright

--- src/main/java/gueiros/lucas/bomsamaritano/service/associado/Associado.java
+++ src/main/java/gueiros/lucas/bomsamaritano/service/associado/Associado.java
@@ -1,4 +1,4 @@
-/* 
+/*******************************************************************************
  * Copyright 2018 Lucas Gueiros 
  *
  * This file is part of BomSamaritanoService.
@@ -14,7 +14,7 @@
  *
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
+ *******************************************************************************/
 package gueiros.lucas.bomsamaritano.service.associado;
 
 import gueiros.lucas.bomsamaritano.service.contribuinte.Contribuinte;
